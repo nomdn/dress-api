be99865e4e471b093b2c3a7d7148ad21b9998156
nomdn <178264759+nomdn@users.noreply.github.com>
2026-06-13 11:36:47 +0800

    feat: 更新.gitignore以排除.edgeone，升级glob依赖到10.5.0

--- dress-api-ssr/nuxt.config.ts
+++ dress-api-ssr/nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  routeRules: {
+    '/**': { ssr: true },
+  },
   modules: ['@element-plus/nuxt'],
 })
