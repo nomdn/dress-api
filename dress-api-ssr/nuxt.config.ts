f6d582c6043825622d31082b8b7b3afc1e280917
nomdn <178264759+nomdn@users.noreply.github.com>
2026-06-15 23:33:30 +0800

    refactor: move @vueuse/core to devDependencies and add @vueuse/nuxt

--- dress-api-ssr/nuxt.config.ts
+++ dress-api-ssr/nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
       ],
     },
   },
-  modules: ['@element-plus/nuxt', '@nuxtjs/sitemap', '@nuxtjs/robots'],
+  modules: ['@element-plus/nuxt', '@nuxtjs/sitemap', '@nuxtjs/robots', '@vueuse/nuxt'],
   site:{
     name: 'Dress API',
     url: config.siteUrl,
@@ -44,4 +44,11 @@ export default defineNuxtConfig({
     remote: config.remote,
     rollback: config.rollback,
   },
+  css: [
+    // 1. 引入 Element Plus 基础样式 (如果你还没有引入的话)
+    'element-plus/dist/index.css',
+    
+    // 2. 🌟 关键：引入 Element Plus 官方的暗黑模式 CSS 变量文件
+    'element-plus/theme-chalk/dark/css-vars.css'
+  ],
 })
